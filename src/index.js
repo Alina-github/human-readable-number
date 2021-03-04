d0e7547cb24c060021bfb61c0463e86e28f385a2
Alina-github <alinarudkovskaa@gmail.com>
2021-03-04 23:22:41 +0300

    init:add resolved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
-}
+    function toReadable(number) {
+        let ones = ['zero', 'one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        let teens = ['ten', 'eleven', 'twelve', 'thirteen', ones[4]+'teen', 'fifteen', ones[6]+'teen', ones[7]+'teen', ones[8]+'teen', ones[9]+'teen'];
+        let dozens = ['', '', 'twenty', 'thirty', 'fourty','fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+        let hundreds = ['hundred']
+        
+        
+        if (number < 10) {
+        return ones[number];}
+        
+        if (number > 9 && number < 20) {
+        return teens[number%10];}
+        
+        
+        if (number > 100) {
+        let b = number%100;
+        
+         if (b < 100 && b%10 !== 0 && !(b > 9 && b < 20) && b > 9) {
+        return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + dozens[Math.floor(b/10)] + " " + ones[b%10];
+        } else if (b > 9 && b < 20) {return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + teens[b%10];}
+        else if (b < 10) {return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + ones[b%10];}
+        else {return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + dozens[Math.floor((number%100)/10)];}
+        }
+        
+        if (number < 100 && number%10 !== 0) {
+        return dozens[Math.floor(number/10)] + " " + ones[number%10];
+        } else {
+        return dozens[number/10];
+        } 
+        }
