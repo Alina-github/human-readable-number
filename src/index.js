34a76ce32f6eb07ad79a3186086ac89723fa66f6
Alina-github <alinarudkovskaa@gmail.com>
2021-03-05 00:07:54 +0300

    fix: add rule for hundreds

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ module.exports = function toReadable(number) {
          if (b < 100 && b%10 !== 0 && !(b > 9 && b < 20) && b > 9) {
         return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + dozens[Math.floor(b/10)] + " " + ones[b%10];
         } else if (b > 9 && b < 20) {return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + teens[b%10];}
+        else if (b = 0) {return ones[Math.floor(number/100)] + " " + hundreds[0];}
         else if (b < 10) {return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + ones[b%10];}
         else {return ones[Math.floor(number/100)] + " " + hundreds[0] + " " + dozens[Math.floor((number%100)/10)];}
         }
